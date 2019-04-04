e4114c9350e19a1a0bdd8e75824aca2a5c6d7ac8
Jordi Armengol Estapé <me@jordiae.com>
2019-04-04 18:28:03 +0200

    Scripts for running experiment with POS.

---
+++ preprocess_tags.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+#SBATCH -p veu # Partition to submit to
+#SBATCH --gres=gpu:0
+#SBATCH --mem=10G # Memory
+#SBATCH --ignore-pbs
+#SBATCH --output=/home/usuaris/veu/jordi.armengol/tfg/new/logs/preprocess-postags-bpe.log
+
+
+WORKING_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/data/iwslt14-preprocessed-joined"
+SRC="de_postags"
+TGT="en"
+
+TRN_PREF="train"
+VAL_PREF="valid"
+TES_PREF="test"
+PYTHON="python"
+FACTOR="de_postags"
+FAIRSEQ_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/src/fairseq-baseline-factored"
+
+DEST_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/data/iwslt14-preprocessed-joined"
+
+stdbuf -i0 -e0 -o0  $PYTHON $FAIRSEQ_DIR/preprocess.py --source-lang $SRC --target-lang $TGT \
+    --trainpref $DEST_DIR/${TRN_PREF}.bpe --validpref $DEST_DIR/${VAL_PREF}.bpe --testpref $DEST_DIR/${TES_PREF}.bpe \
+    --destdir $DEST_DIR  --nwordstgt 32000 --nwordssrc 32000
---
+++ train_tags.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+#SBATCH -p veu # Partition to submit to
+#SBATCH --gres=gpu:1
+#SBATCH --mem=20G # Memory
+#SBATCH --ignore-pbs                                                            
+#SBATCH --output=/home/usuaris/veu/jordi.armengol/tfg/new/logs/train8-factored-bpe-postags.log
+
+WORKING_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/data/iwslt14-preprocessed-joined"
+CP_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/data/iwslt14-preprocessed-joined/checkpoints8"
+PYTHON="python"
+FAIRSEQ_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/src/fairseq-baseline-factored"
+
+source ~/.bashrc
+conda activate env
+
+mkdir -p $CP_DIR
+
+stdbuf -i0 -e0 -o0 $PYTHON $FAIRSEQ_DIR/train.py $WORKING_DIR \
+ --task factored_translation --arch factored_transformer_iwslt_de_en  --optimizer adam --adam-betas '(0.9, 0.98)' --lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates 4000 --lr 0.0005 --min-lr 1e-09 --dropout 0.3 --weight-decay 0.0001 --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --max-tokens 4000  --save-dir $CP_DIR --lang-pairs de-en,de_postags-en --max-update 50000
