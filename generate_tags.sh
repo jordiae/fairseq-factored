d7c10abbc6c17868695c1b0d00f6d182e38a70e9
Jordi Armengol Estapé <me@jordiae.com>
2019-04-08 19:11:06 +0200

    Generate/evaluation script with POS.

--- generate_tags.sh
+++ generate_tags.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:0
 #SBATCH --mem=10G # Memory
 #SBATCH --ignore-pbs                                                            
-#SBATCH --output=/home/usuaris/veu/jordi.armengol/tfg/new/logs/generate9-average-baseline-test-joined-bpe-de2en-best-remove-bpe.log
+#SBATCH --output=/home/usuaris/veu/jordi.armengol/tfg/new/logs/generate8-factored-bpe-postags-remove-bpe.log
 
 
 WORKING_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/data/iwslt14-preprocessed-joined"
@@ -22,4 +22,4 @@ FAIRSEQ_DIR="/home/usuaris/veu/cescola/fairseq"
 #    --beam 5 --batch-size 1 --source-lang ${SRC} --target-lang ${TGT} --task translation --remove-bpe
 
 stdbuf -i0 -e0 -o0 $PYTHON $FAIRSEQ_DIR/generate.py $DEST_DIR --path $CP_DIR/$CP \
-	--beam 5 --batch-size 1 --source-lang ${SRC} --target-lang ${TGT} --task translation --remove-bpe
+	--beam 5 --batch-size 1 --lang-pairs de-en,de_postags-en --task factored_translation --remove-bpe
