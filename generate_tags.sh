2fc2e2f39995055144a00d9ef34e6d548b322ccc
Jordi Armengol Estapé <me@jordiae.com>
2019-04-08 19:12:56 +0200

    Generate/evaluation script with POS.

--- generate_tags.sh
+++ generate_tags.sh
@@ -16,7 +16,7 @@ CP_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/data/iwslt14-preprocessed-joine
 #CP="checkpoint_best.pt"
 CP="model.pt"
 PYTHON="/home/usuaris/veu/cescola/virtualenv-16.0.0/torch/bin/python"
-FAIRSEQ_DIR="/home/usuaris/veu/cescola/fairseq"
+FAIRSEQ_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/src/fairseq-baseline-factored"
 
 #stdbuf -i0 -e0 -o0 $PYTHON $FAIRSEQ_DIR/generate.py $DEST_DIR --path $CP_DIR/$CP \
 #    --beam 5 --batch-size 1 --source-lang ${SRC} --target-lang ${TGT} --task translation --remove-bpe
