d4f2fa6193e9d4f3b2c5f9943e5fa21a45fe898c
Jordi Armengol Estapé <me@jordiae.com>
2019-04-16 19:15:41 +0200

    more experiments

--- average.sh
+++ average.sh
@@ -5,17 +5,18 @@
 #SBATCH --gres=gpu:0
 #SBATCH --mem=10G # Memory
 #SBATCH --ignore-pbs                                                            
-#SBATCH --output=/home/usuaris/veu/jordi.armengol/tfg/new/logs/average7.log
+#SBATCH --output=/home/usuaris/veu/jordi.armengol/tfg/new/logs/average12.log
 
 
 WORKING_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/data/iwslt14-preprocessed-joined"
-SRC="de"
-TGT="en"
 DEST_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/data/iwslt14-preprocessed-joined"
-CP_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/data/iwslt14-preprocessed-joined/checkpoints7"
-CP="checkpoint_best.pt"
-PYTHON="/home/usuaris/veu/cescola/virtualenv-16.0.0/torch/bin/python"
-FAIRSEQ_DIR="/home/usuaris/veu/cescola/fairseq"
+CP_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/data/iwslt14-preprocessed-joined/checkpoints12"
+PYTHON="python"
+FAIRSEQ_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/src/fairseq-baseline-factored"
+
+source ~/.bashrc
+conda activate env
+
 
 stdbuf -i0 -e0 -o0 $PYTHON  $FAIRSEQ_DIR/scripts/average_checkpoints.py --inputs $CP_DIR \
 	--num-epoch-checkpoints 10 --output $CP_DIR/model.pt
--- generate_tags.sh
+++ generate_tags.sh
@@ -5,16 +5,16 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=20G # Memory
 #SBATCH --ignore-pbs                                                            
-#SBATCH --output=/home/usuaris/veu/jordi.armengol/tfg/new/logs/generate8-factored-bpe-postags-remove-bpe.log
+#SBATCH --output=/home/usuaris/veu/jordi.armengol/tfg/new/logs/generate12-factored-bpe-postags-remove-bpe-al-at-reduced-freeze-7.log
 
 
 WORKING_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/data/iwslt14-preprocessed-joined"
 SRC="de"
 TGT="en"
 DEST_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/data/iwslt14-preprocessed-joined"
-CP_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/data/iwslt14-preprocessed-joined/checkpoints8"
-CP="checkpoint_best.pt"
-#CP="model.pt"
+CP_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/data/iwslt14-preprocessed-joined/checkpoints12"
+#CP="checkpoint_best.pt"
+CP="model.pt"
 PYTHON="python"
 FAIRSEQ_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/src/fairseq-baseline-factored"
 
@@ -27,4 +27,4 @@ conda activate env
 #    --beam 5 --batch-size 1 --source-lang ${SRC} --target-lang ${TGT} --task translation --remove-bpe
 
 stdbuf -i0 -e0 -o0 $PYTHON $FAIRSEQ_DIR/generate.py $DEST_DIR --path $CP_DIR/$CP \
-	--beam 5 --batch-size 1 --lang-pairs de-en,de_postags-en --task factored_translation --remove-bpe
+	--beam 5 --batch-size 1 --lang-pairs de-en,de_postags_at-en --task factored_translation --remove-bpe
