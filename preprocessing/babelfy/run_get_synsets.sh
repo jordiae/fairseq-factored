4ec45d8773b5a4a36ce06cfa9b92c86a24104322
Jordi Armengol Estapé <me@jordiae.com>
2019-04-25 20:19:06 +0200

    minor fix in bash script for launching script to get synsets.

--- preprocessing/babelfy/run_get_synsets.sh
+++ preprocessing/babelfy/run_get_synsets.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-#SBATCH -p veu # Partition to submit to
+#SBATCH -p veu-fast # Partition to submit to
 #SBATCH --mem=8G # Memory
 #SBATCH --ignore-pbs                                                            
-####### --output=/home/usuaris/veu/jordi.armengol/tfg/new/logs/get_synsets_de_1.log
+#SBATCH --output=/home/usuaris/veu/jordi.armengol/tfg/new/logs/get_synsets_de_1_valid_test.log
 
 SCRIPT_PATH="/home/usuaris/veu/jordi.armengol/tfg/new/src/fairseq-baseline-factored/preprocessing/babelfy/get_synsets.py"
 PYTHON="python"
@@ -12,5 +12,5 @@ PYTHON="python"
 source ~/.bashrc
 conda activate env
 
-#stdbuf -i0 -e0 -o0 $PYTHON SCRIPT_PATH
-$PYTHON SCRIPT_PATH > "/home/usuaris/veu/jordi.armengol/tfg/new/logs/get_synsets_de_1.log"
+stdbuf -i0 -e0 -o0 $PYTHON $SCRIPT_PATH
+#$PYTHON SSCRIPT_PATH > "/home/usuaris/veu/jordi.armengol/tfg/new/logs/get_synsets_de_1.log"
