8fe2110224543631800a11ca56c16d1554e29aa9
Jordi Armengol Estapé <me@jordiae.com>
2019-05-18 19:06:03 +0200

    Added Stanford model

--- preprocessing/stanford/run_feature_tagger.sh
+++ preprocessing/stanford/run_feature_tagger.sh
@@ -16,4 +16,8 @@ conda activate env
 
 pip install spacy-stanfordnlp
 
+mkdir /home/usuaris/veu/jordi.armengol/stanfordnlp_resources
+wget https://nlp.stanford.edu/software/stanfordnlp_models/0.2.0/en_ewt_models.zip/ -P /home/usuaris/veu/jordi.armengol/stanfordnlp_resources
+unzip /home/usuaris/veu/jordi.armengol/stanfordnlp_resources/en_ewt_models.zip
+
 stdbuf -i0 -e0 -o0 $PYTHON $FEATURE_TAGGER_DIR/feature_tagger.py
