a6d9985b74098a485e5b46d28b2502d37bd085da
Jordi Armengol Estapé <me@jordiae.com>
2019-05-20 19:52:59 +0200

    fixed alignment script

--- preprocessing/stanford/align_flores_sentencepiece.py
+++ preprocessing/stanford/align_flores_sentencepiece.py
@@ -12,13 +12,19 @@ def align_sentencepiece(text_bpe, text_token, text_lemma, text_pos, text_dep, te
     repeated_deps = ''
     repeated_tags = ''
     subword_tags = ''
+    n_lines = len(text_bpe.splitlines())
     for line_bpe, line_token, line_lemma, line_pos, line_dep, line_tag in zip(text_bpe.splitlines(), text_token.splitlines(), text_lemma.splitlines(), text_pos.splitlines(), text_dep.splitlines(), text_tag.splitlines()):
         index_bpe = 0
         for index, token in enumerate(line_token.split()):
+            token = token.replace(chr(8203),'')
             current_word = ''
             counter = 0
             currently_in_space = False
             while not current_word == token:
+                if index_bpe >= len(line_bpe):
+                    print('current_word', current_word)
+                    print('out of bounds', line_token, index_bpe, len(line_bpe)-1)
+                    exit()
                 if line_bpe[index_bpe] == '\u2581' or line_bpe[index_bpe] == ' ':
                     index_bpe += 1
                     if not currently_in_space:
@@ -28,6 +34,14 @@ def align_sentencepiece(text_bpe, text_token, text_lemma, text_pos, text_dep, te
                     current_word += line_bpe[index_bpe]
                     index_bpe += 1
                     currently_in_space = False
+                '''
+                if 'Anti-traditionalism' in line_token:
+                    if 'and' in token:
+                        for c in token:
+                            print(c, ord(c))
+                        #exit()
+                    print(index_bpe, token,len(token), len(current_word), current_word)
+                '''
             if counter == 1:
                 repeated_tokens += token + ' '
                 repeated_lemmas += line_lemma[index] + ' '
@@ -69,14 +83,14 @@ def align_sentencepiece(text_bpe, text_token, text_lemma, text_pos, text_dep, te
             repeated_tokens += '\n'
             repeated_lemmas += '\n'
             repeated_pos += '\n'
-            repeated_deps[-1] = '\n'
+            repeated_deps += '\n'
             repeated_tags += '\n'
             subword_tags += '\n'
     return repeated_tokens, repeated_lemmas, repeated_pos, repeated_deps, repeated_tags, subword_tags
 
 
 def main():
-    for dataset in  ['train', 'valid', 'test']:
+    for dataset in ['train', 'valid', 'test']:
         with open(os.path.join(PATH, dataset + '.bpe.' + LANG), 'r') as file:
             text_bpe = file.read()
         with open(os.path.join(PATH, dataset + '.' + LANG + '_tokens'), 'r') as file:
