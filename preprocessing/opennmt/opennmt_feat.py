db8899e925a6ac1fa6e35d77b756590d1e3fceff
Jordi Armengol Estapé <me@jordiae.com>
2019-05-26 12:18:02 +0200

    Added OpenNMT script for word features.

--- preprocessing/opennmt/opennmt_feat.py
+++ preprocessing/opennmt/opennmt_feat.py
@@ -6,10 +6,12 @@ PATH = '/home/usuaris/veu/jordi.armengol/tfg/new/OpenNMT-py/data/iwslt14'
 def format_opennmt(text_bpe, text_lemma, text_pos, text_dep, text_tag):
     sep = '\uFFE8'
     res = ''
-    for line_bpe, line_lemma, line_pos, line_dep, line_tag in zip(text_bpe.splitlines(), text_lemma.splitlines(), text_pos.splitlines(), text_dep.splitlines(), text_tag.splitlines()):
+    for (index, (line_bpe, line_lemma, line_pos, line_dep, line_tag)) in enumerate(zip(text_bpe.splitlines(), text_lemma.splitlines(), text_pos.splitlines(), text_dep.splitlines(), text_tag.splitlines())):
         for bpe, lemma, pos, dep, tag in zip(line_bpe.split(), line_lemma.split(), line_pos.split(), line_dep.split(), line_tag.split()):
             res += bpe + sep + lemma + sep + pos + sep + dep + sep + tag + ' '
         res = res[:-1] + '\n'
+        if index % 1000 == 0:
+            print('Processed', index+1, 'sentences')
     return res
 
 
@@ -28,7 +30,7 @@ def main():
         with open(os.path.join(PATH, dataset + '.bpe.' + LANG + '_tags'), 'r') as file:
             text_tag = file.read()
         res = format_opennmt(text_bpe, text_lemma, text_pos, text_dep, text_tag)
-        with open(os.path.join(PATH, 'opennmt.' + dataset + '.bpe.' + LANG + 'txt')) as file:
+        with open(os.path.join(PATH, 'opennmt.' + dataset + '.bpe.' + LANG + '.txt')) as file:
             file.write(res)
 
 
