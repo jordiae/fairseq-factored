d331f621439b84333cf00e0cb7f6cb7223d54e3d
Jordi Armengol Estapé <me@jordiae.com>
2019-06-05 12:18:11 +0200

    Using Stanford tokenization

---
+++ preprocessing/stanford/feature_tagger_iwslt14/run_align_tokens.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+#SBATCH -p veu # Partition to submit to
+#SBATCH --mem=10G # Memory
+#SBATCH --ignore-pbs
+#SBATCH --output=/home/usuaris/veu/jordi.armengol/tfg/new/logs/new_islt14_scripts/align_features_de_tokens1.log
+
+
+FEATURE_TAGGER_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/src/fairseq-baseline-factored/preprocessing/stanford/feature_tagger_iwslt14"
+PYTHON="python"
+
+# Activate conda environment
+source ~/.bashrc
+conda activate env
+
+stdbuf -i0 -e0 -o0 $PYTHON $FEATURE_TAGGER_DIR/align_features_iwslt14_bpe_tokens.py
