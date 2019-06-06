635af54e21126e4f117f987bb8c4d7d594faad5a
Jordi Armengol Estapé <me@jordiae.com>
2019-06-06 13:25:21 +0200

    Fixed Stanford alignment issue

--- preprocessing/stanford/feature_tagger_iwslt14/align_features_iwslt14_bpe_tokensS.py
+++ preprocessing/stanford/feature_tagger_iwslt14/align_features_iwslt14_bpe_tokensS.py
@@ -132,23 +132,28 @@ def align_bpe(text_bpe, tags):
         tag_tokens = line_tags.split()
         bpe_index = 0
         tag_index = 0
+        res_line = ''
         while bpe_index < len(bpe_tokens):
             if '@@' in bpe_tokens[bpe_index]:
                 while '@@' in bpe_tokens[bpe_index]:
-                    res += tag_tokens[tag_index] + ' '
+                    res_line += tag_tokens[tag_index] + ' '
                     bpe_index += 1
                     if '@@' not in bpe_tokens[bpe_index]:
-                        res += tag_tokens[tag_index] + ' '
+                        res_line += tag_tokens[tag_index] + ' '
                         bpe_index += 1
                         tag_index += 1
                         break
             else:
-                res += tag_tokens[tag_index] + ' '
+                res_line += tag_tokens[tag_index] + ' '
                 bpe_index += 1
                 tag_index += 1
         if tag_index != len(tag_tokens):
+            print(res_line)
+            print(line_bpe)
+            print(line_tags)
+            print(tag_index, len(tag_tokens))
             raise Exception('Ignored tags in line ' + str(index_line))
-        res += '\n'
+        res += res_line + '\n'
     return res
 
 
