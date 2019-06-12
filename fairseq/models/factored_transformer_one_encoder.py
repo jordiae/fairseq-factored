4a496e99bc359b44e0365cfd5da90280bad1c3bf
Jordi Armengol Estapé <me@jordiae.com>
2019-06-12 11:56:23 +0200

    Added configuration for one encoder concat (lemmas).

--- fairseq/models/factored_transformer_one_encoder.py
+++ fairseq/models/factored_transformer_one_encoder.py
@@ -948,13 +948,13 @@ def factored_one_encoder_iwslt_de_en(args):
 
 @register_model_architecture('factored_transformer_one_encoder', 'factored_transformer_one_encoder_lemmas_stanfordS')
 def factored_one_encoder_iwslt_de_en(args):
-    args.encoder_embed_dim = getattr(args, 'encoder_embed_dim', 1024)
-    args.encoder_ffn_embed_dim = getattr(args, 'encoder_ffn_embed_dim', 2048)
+    args.encoder_embed_dim = getattr(args, 'encoder_embed_dim', 1028)
+    args.encoder_ffn_embed_dim = getattr(args, 'encoder_ffn_embed_dim', 2056)
     args.encoder_attention_heads = getattr(args, 'encoder_attention_heads', 4)
     args.encoder_layers = getattr(args, 'encoder_layers', 6)
-    args.encoder_embed_dim_sizes = {'de_tokensS': 512, 'de_tokensS_lemmas': 512}
-    args.decoder_embed_dim = getattr(args, 'decoder_embed_dim', 1024)
-    args.decoder_ffn_embed_dim = getattr(args, 'decoder_ffn_embed_dim', 2048)
+    args.encoder_embed_dim_sizes = {'de_tokensS': 512, 'de_tokensS_lemmas': 512, 'de_tokensS_subword_tags': 4}
+    args.decoder_embed_dim = getattr(args, 'decoder_embed_dim', 1028)
+    args.decoder_ffn_embed_dim = getattr(args, 'decoder_ffn_embed_dim', 2056)
     args.decoder_attention_heads = getattr(args, 'decoder_attention_heads', 4)
     args.decoder_layers = getattr(args, 'decoder_layers', 6)
     factored_one_encoder_base_architecture(args)
