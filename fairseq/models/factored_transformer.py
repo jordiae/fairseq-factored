6b787678038508208f4cb75a5dba2f1558077db6
Jordi Armengol Estapé <me@jordiae.com>
2019-06-12 11:43:14 +0200

    Added configuration for multiple encoder concat (lemmas).

--- fairseq/models/factored_transformer.py
+++ fairseq/models/factored_transformer.py
@@ -241,3 +241,17 @@ def factored_transformer_iwslt_de_en(args):
     args.decoder_attention_heads = getattr(args, 'decoder_attention_heads', 4)
     args.decoder_layers = getattr(args, 'decoder_layers', 6)
     base_factored_architecture(args)
+
+@register_model_architecture('factored_transformer', 'factored_transformer__iwslt_de_en_lemmas_stanfordS')
+def factored_one_encoder_iwslt_de_en(args):
+    args.encoder_embed_dim = getattr(args, 'encoder_embed_dim', 512)
+    args.encoder_ffn_embed_dim = getattr(args, 'encoder_ffn_embed_dim', 1024)
+    args.encoder_attention_heads = getattr(args, 'encoder_attention_heads', 4)
+    args.encoder_layers = getattr(args, 'encoder_layers', 6)
+    args.factor_encoder_embed_dim = 512
+    args.factor = 'de_tokensS_lemmas'
+    args.decoder_embed_dim = getattr(args, 'decoder_embed_dim', 1024)
+    args.decoder_ffn_embed_dim = getattr(args, 'decoder_ffn_embed_dim', 2048)
+    args.decoder_attention_heads = getattr(args, 'decoder_attention_heads', 4)
+    args.decoder_layers = getattr(args, 'decoder_layers', 6)
+    base_factored_architecture(args)
