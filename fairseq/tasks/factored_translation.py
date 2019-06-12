1f7f1fc6e33b55a731cfed14ebd5fd9dce76b9a7
Jordi Armengol Estapé <me@jordiae.com>
2019-06-12 22:14:54 +0200

    Prototyping masking.

--- fairseq/tasks/factored_translation.py
+++ fairseq/tasks/factored_translation.py
@@ -211,7 +211,7 @@ class FactoredTranslationTask(FairseqTask):
                     #index_to_mask = int(np.round(np.random.uniform(0, len(src_tokens[i])-1)))
                     index_to_mask = torch.LongTensor(1).random_(0, len(src_tokens[i]))[0]
                     print(src_tokens[i][index_to_mask])
-                    src_tokens[i][index_to_mask] = torch.tensor(self.dicts[lang_pair.split('-')[0]].unk())
+                    src_tokens[i][index_to_mask] = 3#torch.tensor(self.dicts[lang_pair.split('-')[0]].unk())
                     print(src_tokens[i][index_to_mask])
                 #if torch.cuda.is_available(): src_tokens.cuda()
 
