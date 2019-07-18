fb3fd5bdb6ee02c5fce2ab0b13e4b0116040ebb1
Jordi Armengol Estapé <me@jordiae.com>
2019-07-18 19:10:06 +0200

    Added tag preprocessing for Nepali

--- flores_scripts/preprocess_flores_enne_tags.sh
+++ flores_scripts/preprocess_flores_enne_tags.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=10G # Memory
 #SBATCH --ignore-pbs
-#SBATCH --output=/home/usuaris/veu/jordi.armengol/tfg/new/logs/flores/en-ne/preprocess1.log
+#SBATCH --output=/home/usuaris/veu/jordi.armengol/tfg/new/logs/flores/en-ne/preprocess2_tags.log
 
 BPESIZE=5000
 WORKING_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/data/flores/data/wiki_ne_en_bpe${BPESIZE}"
@@ -26,6 +26,38 @@ DEST_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/data/flores/data-bin/wiki_${S
 source ~/.bashrc
 conda activate env
 
-stdbuf -i0 -e0 -o0  $PYTHON $FAIRSEQ_DIR/preprocess.py --source-lang $SRC --target-lang $TGT \
+SRC_TAG="en_lemmas"
+
+stdbuf -i0 -e0 -o0  $PYTHON $FAIRSEQ_DIR/preprocess.py --source-lang SRC_TAG --target-lang $TGT \
     --trainpref $WORKING_DIR/${TRN_PREF}.bpe --validpref $WORKING_DIR/${VAL_PREF}.bpe --testpref $WORKING_DIR/${TES_PREF}.bpe \
-    --destdir $DEST_DIR  --joined-dictionary --workers 4
+    --destdir $DEST_DIR  --joined-dictionary --workers 4  --tgtdict $DEST_DIR/dict.ne.txt
+
+SRC_TAG="en_synsets"
+
+stdbuf -i0 -e0 -o0  $PYTHON $FAIRSEQ_DIR/preprocess.py --source-lang SRC_TAG --target-lang $TGT \
+    --trainpref $WORKING_DIR/${TRN_PREF}.bpe --validpref $WORKING_DIR/${VAL_PREF}.bpe --testpref $WORKING_DIR/${TES_PREF}.bpe \
+    --destdir $DEST_DIR  --joined-dictionary --workers 4  --tgtdict $DEST_DIR/dict.ne.txt
+
+SRC_TAG="en_pos"
+
+stdbuf -i0 -e0 -o0  $PYTHON $FAIRSEQ_DIR/preprocess.py --source-lang SRC_TAG --target-lang $TGT \
+    --trainpref $WORKING_DIR/${TRN_PREF}.bpe --validpref $WORKING_DIR/${VAL_PREF}.bpe --testpref $WORKING_DIR/${TES_PREF}.bpe \
+    --destdir $DEST_DIR  --joined-dictionary --workers 4  --tgtdict $DEST_DIR/dict.ne.txt
+
+SRC_TAG="en_deps"
+
+stdbuf -i0 -e0 -o0  $PYTHON $FAIRSEQ_DIR/preprocess.py --source-lang SRC_TAG --target-lang $TGT \
+    --trainpref $WORKING_DIR/${TRN_PREF}.bpe --validpref $WORKING_DIR/${VAL_PREF}.bpe --testpref $WORKING_DIR/${TES_PREF}.bpe \
+    --destdir $DEST_DIR  --joined-dictionary --workers 4  --tgtdict $DEST_DIR/dict.ne.txt
+
+SRC_TAG="en_tags"
+
+stdbuf -i0 -e0 -o0  $PYTHON $FAIRSEQ_DIR/preprocess.py --source-lang SRC_TAG --target-lang $TGT \
+    --trainpref $WORKING_DIR/${TRN_PREF}.bpe --validpref $WORKING_DIR/${VAL_PREF}.bpe --testpref $WORKING_DIR/${TES_PREF}.bpe \
+    --destdir $DEST_DIR  --joined-dictionary --workers 4  --tgtdict $DEST_DIR/dict.ne.txt
+
+SRC_TAG="en_subword_tags"
+
+stdbuf -i0 -e0 -o0  $PYTHON $FAIRSEQ_DIR/preprocess.py --source-lang SRC_TAG --target-lang $TGT \
+    --trainpref $WORKING_DIR/${TRN_PREF}.bpe --validpref $WORKING_DIR/${VAL_PREF}.bpe --testpref $WORKING_DIR/${TES_PREF}.bpe \
+    --destdir $DEST_DIR  --joined-dictionary --workers 4  --tgtdict $DEST_DIR/dict.ne.txt
