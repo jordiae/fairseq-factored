89049b2be02d532d70b3bd882b56c167d50960a1
Jordi Armengol Estapé <me@jordiae.com>
2019-07-18 18:43:20 +0200

    added flores execution scripts

---
+++ flores_scripts/train_flores_enne.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+#SBATCH -p veu # Partition to submit to
+#SBATCH --gres=gpu:1
+#SBATCH --mem=20G # Memory
+#SBATCH --ignore-pbs                                                            
+#SBATCH --output=/home/usuaris/veu/jordi.armengol/tfg/new/logs/flores/en-ne/train1.log
+
+WORKING_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/data/flores/data-bin/wiki_ne_en_bpe5000"
+CP_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/data/flores/data-bin/wiki_ne_en_bpe5000/checkpoints_enne"
+PYTHON="python"
+FAIRSEQ_DIR="/home/usuaris/veu/jordi.armengol/tfg/new/src/fairseq-baseline-factored"
+
+source ~/.bashrc
+conda activate env
+
+mkdir -p $CP_DIR
+
+stdbuf -i0 -e0 -o0 $PYTHON $FAIRSEQ_DIR/train.py $WORKING_DIR \
+--source-lang en --target-lang ne \
+    --arch transformer --share-all-embeddings \
+    --encoder-layers 5 --decoder-layers 5 \
+    --encoder-embed-dim 512 --decoder-embed-dim 512 \
+    --encoder-ffn-embed-dim 2048 --decoder-ffn-embed-dim 2048 \
+    --encoder-attention-heads 2 --decoder-attention-heads 2 \
+    --encoder-normalize-before --decoder-normalize-before \
+    --dropout 0.4 --attention-dropout 0.2 --relu-dropout 0.2 \
+    --weight-decay 0.0001 \
+    --label-smoothing 0.2 --criterion label_smoothed_cross_entropy \
+    --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0 \
+    --lr-scheduler inverse_sqrt --warmup-update 4000 --warmup-init-lr 1e-7 \
+    --lr 1e-3 --min-lr 1e-9 \
+    --max-tokens 4000 \
+    --update-freq 4 \
+    --max-epoch 100 --keep-last-epochs 2 --save-dir $CP_DIR #--save-interval 10
