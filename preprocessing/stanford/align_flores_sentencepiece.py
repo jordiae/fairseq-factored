073ca64649781dbfc90c93109128ed0c6c4b482e
Jordi Armengol Estapé <me@jordiae.com>
2019-07-21 18:37:03 +0200

    Debugging feature alignment Flores

--- preprocessing/stanford/align_flores_sentencepiece.py
+++ preprocessing/stanford/align_flores_sentencepiece.py
@@ -58,13 +58,13 @@ def align_sentencepiece(text_bpe, text_token, text_lemma, text_pos, text_dep, te
                         subword_tags += 'O' + ' '
                         current_word = ''
                         index_bpe += 1
-                        print('OK')
+                        # print('OK')
                         continue
                     index_bpe += 1
                     if not currently_in_space:
                         counter += 1
                     currently_in_space = True
-                elif index+2 < len(line_token.split()) and line_lemma[index+1] == '"' and line_lemma[index+2] == '"':
+                elif index+2 < len(line_token.split()) and line_lemma.split()[index+1] == '"' and line_lemma.split()[index+2] == '"':
                     print('SOM-HI')
                     repeated_tokens += token + line_token.split()[index+1] + line_token.split()[index+2] + ' '
                     repeated_lemmas += line_lemma.split()[index] + line_lemma.split()[index+1] + line_lemma.split()[index+2] + ' '
