2bb7d5f25bf96eb89e6d14f98960aea1efb4af9a
Jordi Armengol Estapé <me@jordiae.com>
2019-07-23 19:50:25 +0200

    Debugging feature alignment Flores

--- preprocessing/stanford/align_flores_sentencepiece.py
+++ preprocessing/stanford/align_flores_sentencepiece.py
@@ -99,7 +99,9 @@ def align_sentencepiece(text_bpe, text_token, text_lemma, text_pos, text_dep, te
                 special = index_bpe < len(line_bpe) and (((line_bpe[index_bpe] in ['"', '.', '?']) or (line_bpe[index_bpe] in ['s'] and line_bpe[index_bpe-2:index_bpe] == 'it'))\
                     or (index_bpe > 2 and (line_bpe[index_bpe] in ['r'] and line_bpe[index_bpe-3:index_bpe] == 'you'))\
                     or (index_bpe > 2 and (line_bpe[index_bpe:index_bpe+3] == 'not' and line_bpe[index_bpe-3:index_bpe] == 'can')) and index + 1 < len(
-                        line_token.split()) and counter == 1)
+                        line_token.split()) and counter == 1) \
+                    or ((index_bpe > 2) and (line_bpe[index_bpe:index_bpe+2] == 'nt' and line_bpe[index_bpe-2:index_bpe] == 'we') and index +1 < len(line_token.split()) and counter == 1)
+
                 if i == 2847:
                     print(index_bpe, line_bpe[index_bpe], special)
             except Exception as e:
