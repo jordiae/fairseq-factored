2e924086e05588bca54ae1250e1ad65d6e3c3422
Jordi Armengol Estapé <me@jordiae.com>
2019-07-23 19:32:45 +0200

    Debugging feature alignment Flores

--- preprocessing/stanford/align_flores_sentencepiece.py
+++ preprocessing/stanford/align_flores_sentencepiece.py
@@ -95,19 +95,20 @@ def align_sentencepiece(text_bpe, text_token, text_lemma, text_pos, text_dep, te
             #if index_bpe < len(line_bpe) and line_bpe[index_bpe] not in [' ', '\u2581'] and index + 1 < len(
             #            line_token.split()) and counter == 1:
 
-            try:
-                special = index_bpe < len(line_bpe) and (((line_bpe[index_bpe] in ['"', '.']) or (line_bpe[index_bpe] in ['s'] and line_bpe[index_bpe-2:index_bpe] == 'it'))\
-                    or (index_bpe > 2 and (line_bpe[index_bpe] in ['r'] and line_bpe[index_bpe-3:index_bpe] == 'you'))\
-                    or (index_bpe > 2 and (line_bpe[index_bpe:index_bpe+3] == 'not' and line_bpe[index_bpe-3:index_bpe] == 'can')) and index + 1 < len(
-                        line_token.split()) and counter == 1)
-                if i == 2847:
-                    print(index_bpe, line_bpe[i], special)
+            #try:
+            special = index_bpe < len(line_bpe) and (((line_bpe[index_bpe] in ['"', '.', '?']) or (line_bpe[index_bpe] in ['s'] and line_bpe[index_bpe-2:index_bpe] == 'it'))\
+                or (index_bpe > 2 and (line_bpe[index_bpe] in ['r'] and line_bpe[index_bpe-3:index_bpe] == 'you'))\
+                or (index_bpe > 2 and (line_bpe[index_bpe:index_bpe+3] == 'not' and line_bpe[index_bpe-3:index_bpe] == 'can')) and index + 1 < len(
+                    line_token.split()) and counter == 1)
+            if i == 2847:
+                print(index_bpe, line_bpe[i], special)
+            '''
             except Exception as e:
                 print(e)
                 print(index_bpe)
                 print(len(line_bpe))
                 exit()
-
+            '''
             if special:
                 #print('hola')
                 #print(ord(text_bpe[index_bpe]))
